40170e1ea2ecc9aed9e595bffcb7b542b6ece59a
huqi <leo-miracle@hotmail.com>
2016-07-08 17:25:03 +0800

    Initial Commit

---
+++ testing-connection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testing-connection
+//
+//  Created by Qi Hu on 8/7/16.
+//  Copyright © 2016 Qi Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testing-connection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testing-connection
+//
+//  Created by Qi Hu on 8/7/16.
+//  Copyright © 2016 Qi Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
