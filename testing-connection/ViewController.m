30784988cdd0527ec1503ea601a6a494ab84134b
shixi_huqi <shixi_huqi@staff.sina.com.cn>
2016-07-15 18:03:07 +0800

    push image onto server end successful. now working on uploading voice data and combine the meta data altogether in one post

--- testing-connection/ViewController.m
+++ testing-connection/ViewController.m
@@ -17,6 +17,9 @@
 @implementation ViewController{
     UITextField *username;
     UITextField *pwd;
+    
+    UIImageView *imageView1;
+    UIImageView *imageView2;
 }
 
 - (void)viewDidLoad {
@@ -40,7 +43,7 @@
     btn.layer.cornerRadius = 7;
     btn.backgroundColor = [UIColor redColor];
     [btn setTitle:@"register" forState:UIControlStateNormal];
-    [btn addTarget:self action:@selector(registerUser) forControlEvents:UIControlEventTouchUpInside];
+    [btn addTarget:self action:@selector(uploadImage) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
     
     UIButton *btn2 = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -48,8 +51,142 @@
     btn2.layer.cornerRadius = 7;
     btn2.backgroundColor = [UIColor greenColor];
     [btn2 setTitle:@"get return value" forState:UIControlStateNormal];
-    [btn2 addTarget:self action:@selector(get_PHP_return_value) forControlEvents:UIControlEventTouchUpInside];
+    [btn2 addTarget:self action:@selector(retriveImage) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn2];
+    
+    imageView1 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"testPhoto"]];
+    imageView1.frame = CGRectMake(50, 400, 100, 100);
+    [self.view addSubview:imageView1];
+    
+}
+
+-(void)uploadImage{
+    NSLog(@"starting uploadiong procedures");
+    
+    // 启动系统风火轮
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    //服务器给的域名
+    NSString *domainStr = @"http://10.209.68.42/insert.php";
+    
+    //假如需要提交给服务器的参数是key＝1,class_id=100
+    //创建一个可变字典
+    NSMutableDictionary *parametersDict = [NSMutableDictionary dictionary];
+    //往字典里面添加需要提交的参数
+    
+    NSString *name = @"destop";
+    NSNumber *ID = [NSNumber numberWithInteger:12345];
+    NSString *description = @"testing photo upload function";
+    
+    [parametersDict setObject:name forKey:@"name"];
+    [parametersDict setObject:ID forKey:@"ID"];
+    [parametersDict setObject:description forKey:@"description"];
+    
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    //these serializers have both the normal type and the json type. determine which one to use
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    //do not user the requestSerializer here for the intended string is not a json string. user this if we are sending over an nsdata->json string
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    
+    
+    [manager POST:domainStr parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+        UIImage *img = [UIImage imageNamed:@"wu"];
+        NSData *data = UIImagePNGRepresentation(img);
+        [formData appendPartWithFileData:data name:@"testingPhoto" fileName:@"wu.png" mimeType:@"image/png"];
+    } progress:^(NSProgress * _Nonnull uploadProgress) {
+        NSLog(@"%lf",1.0 *uploadProgress.completedUnitCount / uploadProgress.totalUnitCount);
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        
+        //json解析
+        NSDictionary *resultDict = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableLeaves error:nil];
+        
+        NSString *resultString = [[NSString alloc]initWithData:responseObject encoding:NSUTF8StringEncoding];
+        
+        NSLog(@"---获取到的json格式的字典--%@",resultDict);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        NSLog(@"failure due to: %@", error.userInfo);
+        
+    }];
+    
+//    [manager POST:domainStr parameters:parametersDict constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+//        UIImage *img = imageView1.image;
+//        NSData *data = UIImageJPEGRepresentation(img, 0.5);
+//        [formData appendPartWithFileData:data name:@"testingPhoto" fileName:@"desktop.jpg" mimeType:@"image/jpg"];
+//    } progress:^(NSProgress * _Nonnull uploadProgress) {
+//        NSLog(@"%lf",1.0 *uploadProgress.completedUnitCount / uploadProgress.totalUnitCount);
+//    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+//        
+//        //json解析
+//        NSDictionary *resultDict = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableLeaves error:nil];
+//        
+//        NSString *resultString = [[NSString alloc]initWithData:responseObject encoding:NSUTF8StringEncoding];
+//        
+//        NSLog(@"---获取到的json格式的字典--%@",resultDict);
+//
+//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+//        NSLog(@"failure due to: %@", error.userInfo);
+//    }];
+    
+}
+
+-(void)retriveImage{
+    NSLog(@"starting retriving procedures");
+    
+    // 启动系统风火轮
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    //服务器给的域名
+    NSString *domainStr = @"http://10.209.68.42/retriveImage.php";
+    
+    //假如需要提交给服务器的参数是key＝1,class_id=100
+    //创建一个可变字典
+    NSMutableDictionary *parametersDict = [NSMutableDictionary dictionary];
+    //往字典里面添加需要提交的参数
+    
+//    NSString *name = @"destop";
+    NSNumber *ID = [NSNumber numberWithInteger:12345];
+//    NSString *description = @"testing photo upload function";
+    
+//    [parametersDict setObject:name forKey:@"name"];
+    [parametersDict setObject:ID forKey:@"ID"];
+//    [parametersDict setObject:description forKey:@"description"];
+    
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    //these serializers have both the normal type and the json type. determine which one to use
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+//    manager.responseSerializer.acceptableContentTypes = [[NSSet alloc] initWithObjects:@"image/tiff", @"image/jpeg", @"image/gif", @"image/png", @"image/ico", @"image/x-icon", @"image/bmp", @"image/x-bmp", @"image/x-xbitmap", @"image/x-win-bitmap", nil];
+    //do not user the requestSerializer here for the intended string is not a json string. user this if we are sending over an nsdata->json string
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    
+    
+    [manager POST:domainStr parameters:parametersDict progress:^(NSProgress * _Nonnull uploadProgress) {
+        NSLog(@"%lf",1.0 *uploadProgress.completedUnitCount / uploadProgress.totalUnitCount);
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        
+        //json解析
+        NSDictionary *resultDict = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableLeaves error:nil];
+        
+        NSString *resultString = [[NSString alloc]initWithData:responseObject encoding:NSUTF8StringEncoding];
+        
+        NSData *retrivedData = [NSData alloc];
+        
+        imageView2.image = [UIImage imageWithData:retrivedData];
+        
+        NSLog(@"---获取到的json格式的字典--%@",resultDict);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        NSLog(@"failure due to: %@", error.userInfo);
+    }];
+    
     
 }
 
